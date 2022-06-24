3648a12c4e996d66d65b8c6d8004ec7f6152656b
Grayson-code <northernstarmusic10@gmail.com>
2022-06-24 18:34:45 +0530

    push a  bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,15 @@ const Home: NextPage = () => {
       </Head>
       <p className="flex h-screen justify-center items-center">
         Under Construction, Check Regularly for Vaccine updates, as currently working on a cure.
-        
+
       </p>
       <p className='fixed bottom-0 left-0 p-2'>Support This Project By donating to this XMR Address:
         <p className='font-bold'>4673HBXWDsJBWaX8eNSuZE3WJsjrsrDU8jMTafFZzvhj9aF4gSaGvewDbKKmBk22LyF8yuVr2trHTLmjyY2aXLzw2e7yQWL</p>
       </p>
-      <p className='fixed bottom-0 right-0 p-2'>Made With definite Seriousness by Adwaith💙</p>
+      <div>
+        <p className='fixed bottom-0 right-0 p-2'>Made With definite Seriousness by <a href='https://twitter.com/NorthernStare'>Adwaith✨</a></p>
+      </div>
+
       <p className='text-white'>Spoiler, It affects your fingers, or finger.</p>
     </div>
   )
