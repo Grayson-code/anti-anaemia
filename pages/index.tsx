e3ee631a25737808fea4f0c6020febf2751ed4bc
Grayson-code <northernstarmusic10@gmail.com>
2022-06-24 19:43:25 +0530

    vsc tasks, responsive layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
         <p className='font-bold'>4673HBXWDsJBWaX8eNSuZE3WJsjrsrDU8jMTafFZzvhj9aF4gSaGvewDbKKmBk22LyF8yuVr2trHTLmjyY2aXLzw2e7yQWL</p>
       </p>
       <div>
-        <p className='fixed bottom-0 right-0 p-2'>Made With definite Seriousness by Adwaith✨</p>
+        <p className='fixed p-2 sm:bottom-0 top-0 lg:top-0 '>Made With definite Seriousness by Adwaith✨</p>
       </div>
 
       <p className='text-white'>Spoiler, It affects your fingers, or finger.</p>
